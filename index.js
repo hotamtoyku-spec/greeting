4dd98d39d7989aa052aed6329441540c2c753f2f
Aziz Rozmetov <argringo97@gmail.com>
2025-11-18 17:13:41 +0500

    Upd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api");
 const cron = require("node-cron");
 
-const TOKEN = "YOUR_BOT_TOKEN";
-const CHAT_ID = "USER_CHAT_ID"; // qaysi odamga yuboriladi
+const TOKEN = "8210850395:AAGaq6nM3Zcc0IS0Y9YLJsV3U-xe4t55Oy4";
+const CHAT_ID = "8210850395"; // qaysi odamga yuboriladi
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 // ❗ Bot sizdan biror xabar olishi uchun
