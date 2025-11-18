1b1a81a8c2bd210dbb9151169b7300bece49f265
Aziz Rozmetov <argringo97@gmail.com>
2025-11-18 17:39:35 +0500

    new:
    git commit -m new
    git commit -m new:

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const TOKEN = "8210850395:AAGaq6nM3Zcc0IS0Y9YLJsV3U-xe4t55Oy4";
 const CHAT_ID = "8210850395"; // qaysi odamga yuboriladi
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// ❗ Bot sizdan biror xabar olishi uchun
 bot.on("message", (msg) => {
   console.log("Yozdi:", msg.text);
+  console.log("Chat ID:", msg.chat.id);
   bot.sendMessage(msg.chat.id, "Xabaringiz qabul qilindi 😊");
 });
 
 // 🔥 Har kuni 09:00 da jo‘natish
 cron.schedule("35 12 * * *", () => {
   bot.sendMessage(CHAT_ID, "Assalomu alaykum! Bugun ahvolingiz yaxshimi? 😊");
+});
+
+
+bot.on("message", (msg) => {
+  console.log(msg.chat.id); // Shu orqali ID chiqadi
 });
