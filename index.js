bbe2aa0f5c16d580f0cb21ee6d93958be4c2876a
Aziz Rozmetov <argringo97@gmail.com>
2025-11-18 17:31:16 +0500

    Up one more time

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ bot.on("message", (msg) => {
 });
 
 // 🔥 Har kuni 09:00 da jo‘natish
-cron.schedule("30 17 * * *", () => {
+cron.schedule("35 12 * * *", () => {
   bot.sendMessage(CHAT_ID, "Assalomu alaykum! Bugun ahvolingiz yaxshimi? 😊");
 });
